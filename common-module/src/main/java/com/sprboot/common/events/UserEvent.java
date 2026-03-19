be6b583cdd3ba87bf5817353ca7126ffede83813
TaJCenG <tajusyed765@gmail.com>
2026-03-19 16:17:46 +0530

    commit4

---
+++ common-module/src/main/java/com/sprboot/common/events/UserEvent.java
@@ -0,0 +1,18 @@
+package com.sprboot.common.events;
+
+public class UserEvent {
+    private Long userId;
+    public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	private String email;
+}
