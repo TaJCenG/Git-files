0eb6f400c279d05387d42a40d84c06aeba647487
TaJCenG <tajusyed765@gmail.com>
2026-03-22 06:03:00 +0530

    commit 9

---
+++ ProjectJava-SpringBoot-Microservices/src/main/java/com/microservices/andresprojectmicroservices/AndresprojectmicroservicesApplication.java
@@ -0,0 +1,13 @@
+package com.microservices.andresprojectmicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndresprojectmicroservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndresprojectmicroservicesApplication.class, args);
+	}
+
+}
---
+++ ProjectJava-SpringBoot-Microservices/src/test/java/com/microservices/andresprojectmicroservices/AndresprojectmicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.andresprojectmicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AndresprojectmicroservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
